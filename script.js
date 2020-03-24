36fb26930e32ce25c10c97e44fc065615d9e2934
NoahPG88 <57874889+NoahPG88@users.noreply.github.com>
2020-03-24 18:00:21 -0500

    cleaned up form validation

--- script.js
+++ script.js
@@ -9,47 +9,29 @@ window.addEventListener("load", function(){
    form.addEventListener("submit", function(event) {
 
       let pilotNameInput = document.getElementById("pilotName").value;
-      if(!pilotNameInput){
+      let copilotNameInput = document.getElementById("copilotName").value;
+      let fuelLevelInput = document.getElementById("fuelLevel").value;
+      let cargoMassInput = document.getElementById("cargoMass").value;
+
+      if(!pilotNameInput || !copilotNameInput || !fuelLevelInput || !cargoMassInput){
          alert("All fields are required");
          event.preventDefault();
-      }
-      if(!isNaN(pilotNameInput)){
+      } else if(!isNaN(pilotNameInput)){
          alert("Pilot name must be a word");
          event.preventDefault();
-      }
-
-      let copilotNameInput = document.getElementById("copilotName").value;
-      if(!copilotNameInput){
-         alert("All fields are required");
-         event.preventDefault();
-      }
-      if(!isNaN(copilotNameInput)){
+      } else if (!isNaN(copilotNameInput)){
          alert("Copilot name must be a word");
          event.preventDefault();
-      }
-
-      let fuelLevelInput = document.getElementById("fuelLevel").value;
-      if(!fuelLevelInput){
-         alert("All fields are required");
-         event.preventDefault();
-      }
-      if(isNaN(fuelLevelInput)){
+      } else if(isNaN(fuelLevelInput)){
          alert("Fuel level must be a number");
          event.preventDefault();
-      }
-
-      let cargoMassInput = document.getElementById("cargoMass").value;
-      if(!cargoMassInput){
-         alert("All fields are required");
-         event.preventDefault();
-      }
-      if(isNaN(cargoMassInput)){
+      } else if(isNaN(cargoMassInput)){
          alert("Cargo mass must be a number");
          event.preventDefault();
       }
 
    event.preventDefault();
-   
+
    });
 
 });
