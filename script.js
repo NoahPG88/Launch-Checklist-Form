306bda7722b63203cbd6ac0e502270f4698c80a1
NoahPG88 <57874889+NoahPG88@users.noreply.github.com>
2020-03-24 23:16:56 -0500

    finished project 3.24.20

--- script.js
+++ script.js
@@ -1,71 +1,77 @@
-// Write your JavaScript code here!
-
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
    let form = document.getElementById("launchForm");
-   form.addEventListener("submit", function(event) {
+   form.addEventListener("submit", function (event) {
 
       let pilotNameInput = document.getElementById("pilotName").value;
       let copilotNameInput = document.getElementById("copilotName").value;
       let fuelLevelInput = document.getElementById("fuelLevel").value;
       let cargoMassInput = document.getElementById("cargoMass").value;
 
-      if(!pilotNameInput || !copilotNameInput || !fuelLevelInput || !cargoMassInput){
+      if (!pilotNameInput || !copilotNameInput || !fuelLevelInput || !cargoMassInput) {
          alert("All fields are required");
          event.preventDefault();
-      } else if(!isNaN(pilotNameInput)){
+      } else if (!isNaN(pilotNameInput)) {
          alert("Pilot name must be a word");
          event.preventDefault();
-      } else if (!isNaN(copilotNameInput)){
+      } else if (!isNaN(copilotNameInput)) {
          alert("Copilot name must be a word");
          event.preventDefault();
-      } else if(isNaN(fuelLevelInput)){
+      } else if (isNaN(fuelLevelInput)) {
          alert("Fuel level must be a number");
          event.preventDefault();
-      } else if(isNaN(cargoMassInput)){
+      } else if (isNaN(cargoMassInput)) {
          alert("Cargo mass must be a number");
          event.preventDefault();
       }
 
-
+      //variables for pre-launch info
       let launchStatusH2 = document.getElementById("launchStatus");
       let faultyItemsDiv = document.getElementById("faultyItems");
       let pilotStatusLi = document.getElementById("pilotStatus");
       let copilotStatusLi = document.getElementById("copilotStatus");
       let fuelStatusLi = document.getElementById("fuelStatus");
       let cargoStatusLi = document.getElementById("cargoStatus");
-   
-      //Changeing the LI
+
+      //Changeing the list elements
       pilotStatusLi.innerHTML = `Pilot ${pilotNameInput} Ready`;
       copilotStatusLi.innerHTML = `Copilot ${copilotNameInput} Ready`;
 
-      if(fuelLevelInput < 10000){
+      //pre-launch fuel and cargo validation
+      if (fuelLevelInput < 10000) {
          faultyItemsDiv.style.visibility = "visible";
          fuelStatusLi.innerHTML = "Insufficient fuel"
          launchStatusH2.innerHTML = "Shuttle not ready for launch";
          launchStatusH2.style.color = "red";
-      } else if (cargoMassInput > 10000){
+      } else if (cargoMassInput > 10000) {
          faultyItemsDiv.style.visibility = "visible";
          cargoStatusLi.innerHTML = "Excessive cargo mass";
          launchStatusH2.innerHTML = "Shuttle not ready for launch";
          launchStatusH2.style.color = "red";
-      } else{
+      } else {
          launchStatusH2.innerHTML = "Shuttle is ready for launch";
          launchStatusH2.style.color = "green";
       }
 
       event.preventDefault();
    });
-});
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+   this.fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+      response.json().then(function (json) {
+         let i = Math.floor(Math.random()*json.length);
+         let planetOb = json[i];
+         let missionTarget = document.getElementById("missionTarget")
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+            <ol style = "list-style-type:none">
+               <li>Name: ${planetOb.name}</li>
+               <li>Diameter: ${planetOb.diameter}</li>
+               <li>Star: ${planetOb.star}</li>
+               <li>Distance from Earth: ${planetOb.distance}</li>
+               <li>Number of Moons: ${planetOb.moons}</li>
+            </ol>
+            <img src="${planetOb.image}">
+         `
+      });
+   });
+});
